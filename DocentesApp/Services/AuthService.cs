28aeb9f11fff6512b9d34713f1cc4f23d65cb23f
CarlosCuautleZac <191G0235@rcarbonifera.tecnm.mx>
2023-05-22 14:52:47 -0600

    services en maui

---
+++ DocentesApp/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocentesApp.Services
+{
+    public class AuthService
+    {
+        //Normalmente tiene 3 metodos clasicos
+        string token;
+
+
+        //Write
+        public async void WriteToken(string token)
+        {
+            this.token = token;
+            await SecureStorage.SetAsync("JwtToken", token);
+        }
+
+        //Read
+
+        public async Task<string> ReadToken()
+        {
+            token = await SecureStorage.GetAsync("JwtToken");
+            return token;
+        }
+
+        //Remove
+        public void RemoveToken()
+        {
+            SecureStorage.Remove("JwtToken");
+            token = null;
+            token = null;
+        }
+    }
+}
---
+++ DocentesApp/Services/LoginService.cs
@@ -0,0 +1,48 @@
+﻿using DocentesApp.Models.DTOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocentesApp.Services
+{
+    public class LoginService
+    {
+        public string url = "https://docentes.itesrc.net/";
+        HttpClient client;
+        public LoginService()
+        {
+            //Le paso la url al httpclient
+            client= new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public async Task<bool> IniciarSesion(LoginDTO login)
+        {
+            if (string.IsNullOrWhiteSpace(login.Username)||string.IsNullOrWhiteSpace(login.Password))
+            {
+                throw new ArgumentException("Escriba el nombre de usuario o contraseña");
+            }
+
+            var request = await client.PostAsJsonAsync("api/login", login);
+            if (request.IsSuccessStatusCode)
+            {
+
+                //read token
+                var token = await request.Content.ReadFromJsonAsync<string>();
+
+
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
